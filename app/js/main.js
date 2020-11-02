fc17238e40a77740ab75834aabc0fbcbce8cb2aa
4EPSI <mnatsakanyan@yual.io>
2020-11-02 14:54:35 +0200

    slider item

---
+++ app/js/main.js
@@ -0,0 +1,13 @@
+// Home slider
+$('.home_slider').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    dots: true,
+    mouseDrag: false,
+    items: 1,
+    navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ]
+});
