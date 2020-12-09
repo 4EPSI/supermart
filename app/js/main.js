c9524fed31a6603a8c4c3c0e14e67894248215fd
4EPSI <mnatsakanyan@yual.io>
2020-12-09 21:52:31 +0200

    counter

--- app/js/main.js
+++ app/js/main.js
@@ -101,3 +101,100 @@ $('.brand_partners').owlCarousel({
     mouseDrag: false,
     items: 6
 });
+
+// Counter
+$("#getting-started")
+.countdown("2020/12/31", function(event) {
+  $('#day').text(
+    event.strftime('%D')
+  );
+  $('#hours').text(
+      event.strftime('%H')
+    );
+    $('#minutes').text(
+      event.strftime('%M')
+    );
+    $('#seconds').text(
+      event.strftime('%S')
+    );
+});
+// function getTimeRemaining(endtime) {
+//     var t = Date.parse(endtime) - Date.parse(new Date());
+//     var seconds = Math.floor((t / 1000) % 60);
+//     var minutes = Math.floor((t / 1000 / 60) % 60);
+//     var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+//     var days = Math.floor(t / (1000 * 60 * 60 * 24));
+//     return {
+//       'total': t,
+//       'days': days,
+//       'hours': hours,
+//       'minutes': minutes,
+//       'seconds': seconds
+//     };
+//   }
+
+//   function initializeClock(id, endtime) {
+//     var clock = document.getElementById(id);
+//     var daysSpan = clock.querySelector('.days');
+//     var hoursSpan = clock.querySelector('.hours');
+//     var minutesSpan = clock.querySelector('.minutes');
+//     var secondsSpan = clock.querySelector('.seconds');
+
+//     function updateClock() {
+//       var t = getTimeRemaining(endtime);
+
+//       daysSpan.innerHTML = t.days;
+//       hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+//       minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+//       secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+//       if (t.total <= 0) {
+//         clearInterval(timeinterval);
+//       }
+//     }
+
+//     updateClock();
+//     var timeinterval = setInterval(updateClock, 1000);
+//   }
+
+//   var deadline = new Date(Date.parse(new Date()) + 12 * 24 * 60 * 60 * 1000); // for endless timer
+//   initializeClock('countdown', deadline);
+
+
+
+
+
+
+
+//TEST 50%
+// Set the date we're counting down to
+// var countDownDate = new Date("December 31, 2020 24:00:00").getTime();
+
+// // Update the count down every 1 second
+// var x = setInterval(function() {
+
+//   // Get todays date and time
+//   var now = new Date().getTime();
+
+//   // Find the distance between now an the count down date
+//   var distance = countDownDate - now;
+
+//   // Time calculations for days, hours, minutes and seconds
+//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+//   // Display the result in the element with id="demo"
+//   document.getElementById("demo").innerHTML = days + "days " + hours + "hours "
+//   + minutes + "minutes " + seconds + "seconds ";
+
+//   // If the count down is finished, write some text
+//   if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("demo").innerHTML = "EXPIRED";
+//   }
+// }, 1000);
+
+
+
